3d0128bd30965589684e6f813b403aa13675ef32
Ayush Chaurasia <ayushthegreat220@gmail.com>
2023-12-28 14:04:52 +0530

    Adding

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Footer from "./Footer";
+
+
+
+function About() {
+return (
+<div>
+<div className="about">
+    
+    <h2>Food App From Ayush</h2>
+    </div>
+    <div>
+        <Footer />
+    </div>
+</div>)
+};
+
+export default About;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <h3>Copyright @ 2023 Ayush</h3>
+    </div>
+  )
+}
+
+export default Footer;
